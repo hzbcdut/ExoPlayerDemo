84a9fd98b9485b9b584611de057bdd367f222943
huzhengbiao <hzhengbiao@126.com>
2018-10-22 16:51:02 +0800

    ExoPlayer连续播放

--- app/src/main/java/com/example/huzhengbiao/exoplayerdemo/MainActivity.java
+++ app/src/main/java/com/example/huzhengbiao/exoplayerdemo/MainActivity.java
@@ -1,13 +1,142 @@
 package com.example.huzhengbiao.exoplayerdemo;
 
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer mExoPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initPlayer();
+
+        setListener();
+    }
+
+    private void initView() {
+        mPlayerView = findViewById(R.id.player_view);
+    }
+
+    private void initPlayer() {
+        //
+        TrackSelection.Factory adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter());
+        TrackSelector trackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+        mPlayerView.setPlayer(mExoPlayer);
+
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                this, Util.getUserAgent(this, "yourApplicationName"), new DefaultBandwidthMeter());
+        // 设置播放源
+        ExtractorMediaSource.Factory factory = new ExtractorMediaSource.Factory(dataSourceFactory);
+        MediaSource[] mediaSources = new MediaSource[4];
+        String url = "https://baobab.kaiyanapp.com/api/v1/playUrl?vid=132872&resourceType=video&editionType=default&source=aliyun&ptl=true";
+
+        String url2 = "http://uc.cdn.kaiyanapp.com/1490499356527_f752d403_1280x720.mp4?t=1540199953&k=c8500162978e24b8";
+        mediaSources[0] = factory.createMediaSource(Uri.parse(url));
+        mediaSources[1] = factory.createMediaSource(Uri.parse(url2));
+        mediaSources[2] = factory.createMediaSource(Uri.parse(url));
+        mediaSources[3] = factory.createMediaSource(Uri.parse(url2));
+
+
+        ConcatenatingMediaSource concatenatingMediaSource = new ConcatenatingMediaSource(mediaSources);
+        mExoPlayer.prepare(concatenatingMediaSource);
+
+        mExoPlayer.setPlayWhenReady(true);
+    }
+
+
+    private void setListener() {
+        mExoPlayer.addListener(new Player.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+            }
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+
+            }
+
+            @Override
+            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+
+                Log.d("debug", " --< error  = " + error);
+            }
+
+            @Override
+            public void onPositionDiscontinuity(int reason) {
+
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+            }
+
+            @Override
+            public void onSeekProcessed() {
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mExoPlayer.release();
     }
 }
